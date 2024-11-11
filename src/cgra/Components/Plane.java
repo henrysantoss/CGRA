6c02e0bf87362494953d561765418f76c9260c3c
Henry <henrysantoss29@gmail.com>
2024-11-11 17:20:48 -0300

    Ajuste PNG Avião

--- src/cgra/Components/Plane.java
+++ src/cgra/Components/Plane.java
@@ -1,5 +1,7 @@
 package cgra.Components;
 
+import java.awt.Color;
+
 import cgra.Math.MathProcessor;
 
 public class Plane {
@@ -11,10 +13,13 @@ public class Plane {
 	private double angle;
 	private double speed;
 	private double direction;
+	private Color planeColor;
 
-	public Plane() {}
+	public Plane() {
+	}
 
-	public Plane(int code, double x, double y, double radius, double angle, double speed, double direction) {
+	public Plane(int code, double x, double y, double radius, double angle, double speed, double direction,
+			Color planeColor) {
 		this.code = code;
 		this.x = x;
 		this.y = y;
@@ -22,11 +27,13 @@ public class Plane {
 		this.angle = angle;
 		this.speed = speed;
 		this.direction = direction;
+		this.planeColor = planeColor;
 	}
 
 	public int getCode() {
 		return code;
 	}
+
 	public void setCode(int code) {
 		this.code = code;
 	}
@@ -34,6 +41,7 @@ public class Plane {
 	public double getX() {
 		return x;
 	}
+
 	public void setX(double x) {
 		this.x = x;
 	}
@@ -41,6 +49,7 @@ public class Plane {
 	public double getY() {
 		return y;
 	}
+
 	public void setY(double y) {
 		this.y = y;
 	}
@@ -48,6 +57,7 @@ public class Plane {
 	public double getRadius() {
 		return radius;
 	}
+
 	public void setRadius(double radius) {
 		this.radius = radius;
 	}
@@ -55,6 +65,7 @@ public class Plane {
 	public double getAngle() {
 		return angle;
 	}
+
 	public void setAngle(double angle) {
 		this.angle = angle;
 	}
@@ -62,6 +73,7 @@ public class Plane {
 	public double getSpeed() {
 		return speed;
 	}
+
 	public void setSpeed(double speed) {
 		this.speed = speed;
 	}
@@ -69,13 +81,23 @@ public class Plane {
 	public double getDirection() {
 		return direction;
 	}
+
 	public void setDirection(double direction) {
 		this.direction = direction;
+	}
+
+	public Color getPlaneColor() {
+		return planeColor;
+	}
+
+	public void setPlaneColor(Color planeColor) {
+		this.planeColor = planeColor;
 	}
 
 	public void convertToCartesian() {
 		MathProcessor.convertPolarToCartesian(this);
 	}
+
 	public void convertToPolar() {
 		MathProcessor.convertCartesianToPolar(this);
 	}
